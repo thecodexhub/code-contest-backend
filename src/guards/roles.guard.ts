4a4487bfc403f7f6006c201bbe4a48371d074aca
Sandip <sandipramanik07@gmail.com>
2023-12-04 20:01:37 +0530

    feat: add roles guard and decorator

---
+++ src/guards/roles.guard.ts
@@ -0,0 +1,25 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { Roles } from '../decorators/roles.decorator';
+import { Request } from 'express';
+import { User } from '../user/enitities/user.entity';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const roles = this.reflector.get(Roles, context.getHandler());
+    if (!roles) return true;
+
+    const request = context.switchToHttp().getRequest<Request>();
+    const user = (request.user as User) || { role: 'USER' };
+
+    console.log(user.role);
+
+    return roles.includes(user.role);
+  }
+}
